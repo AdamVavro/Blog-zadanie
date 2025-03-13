16daa5c03598e5d8dca236c04b8abfcdc1984b27
AdamVavro <xvavroa@gmail.com>
2025-03-13 23:01:13 +0100

    vytvorený súbor [id].js s funkciami pre načítanie a zobrazenie detailu článku

---
+++ pages/posts/[id].js
@@ -0,0 +1,28 @@
+import { getBlog } from '../../lib/api';
+
+//Načíta detail článku z API, vráti objekt s props, ktoré budú poslané do komponentu Post
+export async function getServerSideProps({ params }) {
+  //console.log('Params ID:', params.id);
+  const blog = await getBlog(params.id);
+  return {
+    props: {
+      blog,
+    },
+  };
+}
+
+//Zobrazí titul a obsah článku
+export default function Post({ blog }) {
+  //console.log('Blog prop:', blog);
+
+  if (!blog) {
+    return <div>Blog not found</div>;
+  }
+
+   return (
+    <div>
+      <h1>{blog.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: blog.main_content }} />
+    </div>
+  );
+}
