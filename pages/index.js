8a4b7b35f5df45a3ea9cb60313b4727630cac4c1
AdamVavro <xvavroa@gmail.com>
2025-03-14 11:04:22 +0100

    stránkovanie na homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { getBlogs } from '../lib/api';
 import Link from 'next/link';
-import { formatDate } from '../lib/utils';
+import { formatDate, getExcerpt } from '../lib/utils';
 
 //Načíta dáta z API, vráti objekt s props, ktoré budú poslané do komponentu Home
-export async function getStaticProps() {
+export async function getServerSideProps({ query }) {
+
+  //stránkovanie
+  const page = parseInt(query.page) || 1; // Stránka z query parametra alebo 1
+  const pageSize = 15; // Počet článkov na stránku
   const blogs = await getBlogs();
 
   // zmena formátu dátumu a zoradenie článkov podľa dátumu publikovania (created_at)
@@ -11,28 +15,82 @@ export async function getStaticProps() {
      return new Date(formatDate(b.created_at)) - new Date(formatDate(a.created_at));
   });
 
+  //Vypočítava indexy pre články, ktoré sa majú zobraziť na aktuálnej stránke.
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedBlogs = sortedBlogs.slice(startIndex, endIndex);//Vytvára pole paginatedBlogs, ktoré obsahuje len články pre aktuálnu stránku.
+
+  const totalPages = Math.ceil(sortedBlogs.length / pageSize);
+
   return {
     props: {
-      blogs: sortedBlogs,
+      blogs: paginatedBlogs,
+      currentPage: page,
+      totalPages: totalPages,
     },
     //revalidate: 10, // Voliteľné: revalidácia dát každých 10 sekúnd
   };
 }
 
 //Prijíma props blogs a vytvára odkaz na detail článku
-export default function Home({ blogs }) {
+export default function Home({ blogs, currentPage, totalPages }) {
+  const getPageNumbers = () => {
+    const pages = [];
+    const maxPagesToShow = 10;
+    let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+    let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+  
+    if (endPage - startPage + 1 < maxPagesToShow) {
+      startPage = Math.max(1, endPage - maxPagesToShow + 1);
+    }
+  
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i);
+    }
+    return pages;
+  };
+
   return (
     <div>
+
       <h1>Blog</h1>
+
+      <p>Number of links: {blogs.length}</p>
+
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
             <Link href={`/posts/${blog.id}`}>
             {blog.title} ({formatDate(blog.created_at)})
             </Link>
+            <p>{getExcerpt(blog.main_content)}</p>
           </li>
         ))}
       </ul>
+
+       <div>
+        {currentPage !== 1 && <Link href={`/?page=1`}>First</Link>}    
+        {currentPage > 1 && (
+          <Link href={`/?page=${currentPage - 1}`}>{'<'}</Link>
+        )}
+        {getPageNumbers().map((pageNumber) => (
+          <Link
+            key={pageNumber}
+            href={`/?page=${pageNumber}`}
+            style={{
+              fontWeight: currentPage === pageNumber ? 'bold' : 'normal',
+            }}
+          >
+            {pageNumber}
+          </Link>
+        ))}
+        {currentPage < totalPages && (
+          <Link href={`/?page=${currentPage + 1}`}>{'>'}</Link>
+        )}
+        {currentPage !== totalPages && (
+          <Link href={`/?page=${totalPages}`}>Last</Link>
+        )}
+      </div>
     </div>
   );
 }
