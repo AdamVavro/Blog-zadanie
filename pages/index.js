9c28bf36e8e37a11140fe967dc62693d52720f3e
AdamVavro <xvavroa@gmail.com>
2025-03-14 09:50:27 +0100

    zobrazenie dátumu na index.js a chronologické zoradenie odkazov

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import { getBlogs } from '../lib/api';
 import Link from 'next/link';
+import { formatDate } from '../lib/utils';
 
 //Načíta dáta z API, vráti objekt s props, ktoré budú poslané do komponentu Home
 export async function getStaticProps() {
   const blogs = await getBlogs();
+
+  // zmena formátu dátumu a zoradenie článkov podľa dátumu publikovania (created_at)
+  const sortedBlogs = blogs.sort((a, b) => {
+     return new Date(formatDate(b.created_at)) - new Date(formatDate(a.created_at));
+  });
+
   return {
     props: {
-      blogs,
+      blogs: sortedBlogs,
     },
     //revalidate: 10, // Voliteľné: revalidácia dát každých 10 sekúnd
   };
@@ -20,7 +27,9 @@ export default function Home({ blogs }) {
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
-            <Link href={`/posts/${blog.id}`}>{blog.title}</Link>
+            <Link href={`/posts/${blog.id}`}>
+            {blog.title} ({formatDate(blog.created_at)})
+            </Link>
           </li>
         ))}
       </ul>
