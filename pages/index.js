dc9ce16cadd803fcb96f12543dab3db241591a98
AdamVavro <xvavroa@gmail.com>
2025-03-16 22:26:37 +0100

    index.js, [id].js style

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export async function getServerSideProps({ query }) {
 
   //stránkovanie
   const page = parseInt(query.page) || 1; // Stránka z query parametra alebo 1
-  const pageSize = 15; // Počet článkov na stránku
+  const pageSize = 6; // Počet článkov na stránku
   const blogs = await getBlogs();
 
   // zmena formátu dátumu a zoradenie článkov podľa dátumu publikovania (created_at)
@@ -58,47 +58,60 @@ export default function Home({ blogs, currentPage, totalPages }) {
   };
 
   return (
-    <div>
-
-      <h1>Blog</h1>
-
-      {/* <p>Number of links: {blogs.length}</p> */}
-
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/posts/${blog.id}?page=${currentPage}`}>
-            {blog.title} ({formatDate(blog.created_at)})
-            </Link>
-            <p>{blog.summary}</p>
-            <p>{formatCategories(blog.category)}</p>
-          </li>
-        ))}
-      </ul>
-
-        {/* stránkovanie */}
-       <div>
-        {currentPage !== 1 && <Link href={`/?page=1`}>First</Link>}    
-        {currentPage > 1 && (
-          <Link href={`/?page=${currentPage - 1}`}>{'<'}</Link>
-        )}
-        {getPageNumbers().map((pageNumber) => (
-          <Link
-            key={pageNumber}
-            href={`/?page=${pageNumber}`}
-            style={{
-              fontWeight: currentPage === pageNumber ? 'bold' : 'normal',
-            }}
-          >
-            {pageNumber}
-          </Link>
-        ))}
-        {currentPage < totalPages && (
-          <Link href={`/?page=${currentPage + 1}`}>{'>'}</Link>
-        )}
-        {currentPage !== totalPages && (
-          <Link href={`/?page=${totalPages}`}>Last</Link>
-        )}
+    <div className='min-h-screen bg-background font-sans antialiased  max-w-6xl m-auto '>
+      <div className="container mx-auto px-5 mb-10">
+        <header className='flex items-center justify-between mt-8 md:mt-16 mb-12'>
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight">Blog</h1>
+        </header>
+        {/* <p className="text-sm text-gray-500 mb-2">Number of links: {blogs.length}</p> */}
+        <main>
+          <ul>
+            <div className="grid grid-cols-1 gap-16 lg:gap-28 md:grid-cols-2 md:my-16 my-8">
+              {blogs.map((blog) => (
+                <div className="break-words">
+                  <li key={blog.id} className='grid grid-cols-1 gap-3 md:col-span-2 mt-4'>
+                    <img src={blog.user.profile_pic} alt="Profile Picture" className="w-full h-[281px] object-cover mb-2" />
+                    <Link href={`/posts/${blog.id}?page=${currentPage}`}className="font-sans font-semibold tracking-tighter text-primary text-2xl md:text-3xl">
+                    {blog.title} 
+                    </Link>
+                    <p className='prose lg:prose-lg italic tracking-tighter text-muted-foreground text-gray-500'>{formatDate(blog.created_at)}</p>
+                    <p className="text-gray-500">{blog.summary}</p>
+                    <p className="text-sm text-gray-500">{formatCategories(blog.category)}</p>
+                  </li>
+                </div>
+              ))}
+            </div>
+          </ul>
+          <nav className='mx-auto flex w-full justify-center'>{/* stránkovanie */}
+            {currentPage !== 1 && 
+              <Link href={`/?page=1`} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-gray-500  hover:text-accent-foreground h-10 px-4 py-2 gap-1 pr-2.5">{'<<'}&nbsp;&nbsp;</Link>}    
+            {currentPage > 1 && (
+              <Link href={`/?page=${currentPage - 1}`} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-gray-500  hover:text-accent-foreground h-10 px-4 py-2 gap-1 pr-2.5">{'< '}Previous&nbsp;&nbsp;</Link>
+            )}
+            {getPageNumbers().map((pageNumber) => (
+              <Link
+                key={pageNumber}
+                href={`/?page=${pageNumber}`}
+                className={`inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-gray-500  hover:text-accent-foreground h-10 w-10 ${
+                  currentPage === pageNumber
+                    ? 'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-gray-500 bg-background hover:bg-accent hover:text-accent-foreground h-10 w-10' // Štýly pre aktívnu stránku
+                    : ''
+                }`}
+              >
+                {pageNumber}
+              </Link>
+            ))}
+            {currentPage < totalPages && (
+              <Link href={`/?page=${currentPage + 1}`} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-gray-500 hover:text-accent-foreground h-10 px-4 py-2 gap-1 pr-2.5">Next{' >'}&nbsp;</Link>
+            )}
+            {currentPage !== totalPages && (
+              <Link href={`/?page=${totalPages}`} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-gray-500  hover:text-accent-foreground h-10 px-4 py-2 gap-1 pr-2.5">{'>>'}&nbsp;</Link>
+            )}
+          </nav>
+        </main>
+        <footer className="mt-8 md:mt-16 mb-12">
+          <div className="text-sm text-muted-foreground text-gray-500">© xvavroa 2025</div>
+        </footer>  
       </div>
     </div>
   );
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -35,7 +35,10 @@ export default function Post({ blog }) {
         h1.remove();
     }
 
-    const restContent = tempDiv.innerHTML.trim();
+    let restContent = tempDiv.innerHTML.trim();
+
+    // Pridaj className k elementom <p>
+    restContent = restContent.replace(/<p>/g, '<p class="my-6 text-[#D1D5DB] tracking-tighter lg:prose-xl">');
 
     return {
         h1Content,
@@ -46,13 +49,36 @@ export default function Post({ blog }) {
   const { h1Content, restContent } = extractContent(blog.main_content);
 
    return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: h1Content }} />
-      <img src={blog.featured_image} alt={blog.title} />
-      <div dangerouslySetInnerHTML={{ __html: restContent }} />
-      <p>Published: {formatDate(blog.created_at)}</p>
-      <Link href="/">Home</Link>
-      <Link href={`/?page=${page}`}>Back</Link>
+    <div className='min-h-screen bg-background font-sans antialiased  max-w-6xl m-auto '>
+      <div className="container mx-auto px-5">
+        <header className='flex items-center justify-between mt-8 md:mt-16 mb-12'>
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight">Blog</h1>
+          <nav>
+            <div className="hidden md:flex items-center">
+              <div className="ml-4 md:ml-8">
+                <Link href="/" className="hover:text-gray-600">Home</Link>
+              </div>
+              <div className="ml-4 md:ml-8">
+                <Link href={`/?page=${page}`} className="hover:text-gray-600">Back</Link>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main className="max-w-prose mx-auto text-xl">
+          <div>
+            <div classname="prose lg:prose-xl dark:prose-invert mx-auto lg:prose-h1:text-4xl mb-10 lg:mt-20 break-words">
+              <h1 className="text-4xl md:text-4xl font-bold tracking-tighter leading-tight mb-8 text-white">
+                <div dangerouslySetInnerHTML={{ __html: h1Content }} />
+              </h1>
+              <div className="blog-content mx-auto">
+                <img src={blog.featured_image} alt={blog.title} className="mb-10"/>
+                <div dangerouslySetInnerHTML={{ __html: restContent }} />
+                <p className="prose lg:prose-lg italic tracking-tighter text-muted-foreground text-gray-500 my-10">{formatDate(blog.created_at)}</p>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
