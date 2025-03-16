5619692a830975aaf31fdc16aee886341eb9c99b
AdamVavro <xvavroa@gmail.com>
2025-03-16 22:51:46 +0100

    vytvorený súbor 404.js

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+// pages/404.js
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-background">
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">404 - Stránka nenájdená</h1>
+      <p className="text-gray-600 dark:text-gray-400 mb-8">Prepáčte, ale stránka, ktorú hľadáte, neexistuje.</p>
+      <Link href="/" className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 w-45">
+        Späť na úvodnú stránku
+      </Link>
+    </div>
+  );
+}
